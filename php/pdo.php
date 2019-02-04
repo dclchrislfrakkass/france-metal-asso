01ad297edae08a7c87e3ffc57faecfdd0b2600df
ChrisL_frakkass <dcl.chrisl@18pixel.fr>
2019-02-04 15:36:23 +0100

    add json and

---
+++ php/pdo.php
@@ -0,0 +1,19 @@
+<?php
+
+try {
+$json =file_get_contents('pdo.JSON');
+$dec=json_decode($json, true);
+
+$bd=new PDO("mysql:host=".$dec['host'].";dbname=".$dec['dbName'], $dec['user'] , $dec['pass']);
+//stock url 51.254.203.143
+$bd->exec('SET NAMES utf8');
+    
+// echo 'Connexion OK';
+}
+catch (Exception $e)
+{
+        die('Erreur : ' . $e->getMessage());
+}
+?>
+
+
