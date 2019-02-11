cdb8c9f0833032e5ed71db358c956ad58bd201bb
dclonyx <dcl.fabieno@18pixel.fr>
2019-02-11 11:56:38 +0100

    mise en page groupe

--- php_work/affichage_vote.php
+++ php_work/affichage_vote.php
@@ -3,151 +3,41 @@
 require '../php/pdo.php';
 // Requete affichage score vote total:
 
-$req = $bd->query("SELECT nomGroupe_Groupe, nomStyleSecondaire_StyleSecondaire, nomStylePrincipal_StylePrincipal, note_Album, Pseudo_membre FROM groupe
+$req = $bd->query("SELECT * FROM groupe
 NATURAL JOIN album
 NATURAL JOIN stylesecondaire
 NATURAL JOIN styleprincipal
 NATURAL JOIN a_voté_pour
-NATURAL JOIN membre
-ORDER BY note_Album");
+GROUP BY nomGroupe_Groupe
+ORDER BY note_Album DESC");
 
 while ($row = $req->fetch()){
+    $chaine='';
+    $chaine2= '';
+    $req2 = $bd->prepare("SELECT * FROM groupe
+    NATURAL JOIN album
+    NATURAL JOIN a_voté_pour
+    NATURAL JOIN membre
+    WHERE idAlbum_Album = :idalbum
+    ORDER BY note_Album DESC
+    LIMIT 3");
+    $req2->execute(array(
+        'idalbum' => $row['idAlbum_Album']
+    ));
+    while ($row2 = $req2->fetch()){
+        // var_dump($req2);
+        $chaine = $row2['Pseudo_membre']." ".$chaine;
+        $chaine2 = $row2['idMembre_membre']." ".$chaine2;
+
+    }
+
     echo "<p> Nom du Groupe : ".$row['nomGroupe_Groupe']."</p>";
+    echo "<p> id album: ".$row['idAlbum_Album']."</p>";
+    echo "<p> id membre : ".$chaine2."</p>";
     echo "<p> Style : ".$row['nomStyleSecondaire_StyleSecondaire']."</p>";
     echo "<p> Catégorie : ".$row['nomStylePrincipal_StylePrincipal']."</p>";
     echo "<p> Nombre de vote : ".$row['note_Album']."</p>";
-    echo "<p> Pseudo des votants : ".$row['Pseudo_membre']."</p>";
-}
-$req ->closeCursor();
+    echo "<p> Pseudo des votants : ".$chaine."</p><br>";
 
-
-
-// Requete affichage score votes catégorie STYLE METAL EXTREME:
-
-$req = $bd->query("SELECT nomGroupe_Groupe, nomStyleSecondaire_StyleSecondaire, nomStylePrincipal_StylePrincipal, note_Album, Pseudo_membre FROM groupe
-NATURAL JOIN album
-NATURAL JOIN stylesecondaire
-NATURAL JOIN styleprincipal
-NATURAL JOIN a_voté_pour
-NATURAL JOIN membre
-WHERE idStyleprincipal_StylePrincipal='1'
-ORDER BY note_Album");
-
-while ($row = $req->fetch()){
-    echo "<p> Nom du Groupe : ".$row['nomGroupe_Groupe']."</p>";
-    echo "<p> Style : ".$row['nomStyleSecondaire_StyleSecondaire']."</p>";
-    echo "<p> Catégorie : ".$row['nomStylePrincipal_StylePrincipal']."</p>";
-    echo "<p> Nombre de vote : ".$row['note_Album']."</p>";
-    echo "<p> Pseudo des votants : ".$row['Pseudo_membre']."</p>";
-}
-$req ->closeCursor();
-
-
-
-// Requete affichage score votes catégorie STYLE METAL DARK AMBIENT ATMO:
-
-$req = $bd->query("SELECT nomGroupe_Groupe, nomStyleSecondaire_StyleSecondaire, nomStylePrincipal_StylePrincipal, note_Album, Pseudo_membre FROM groupe
-NATURAL JOIN album
-NATURAL JOIN stylesecondaire
-NATURAL JOIN styleprincipal
-NATURAL JOIN a_voté_pour
-NATURAL JOIN membre
-WHERE idStyleprincipal_StylePrincipal='2'
-ORDER BY note_Album");
-
-while ($row = $req->fetch()){
-    echo "<p> Nom du Groupe : ".$row['nomGroupe_Groupe']."</p>";
-    echo "<p> Style : ".$row['nomStyleSecondaire_StyleSecondaire']."</p>";
-    echo "<p> Catégorie : ".$row['nomStylePrincipal_StylePrincipal']."</p>";
-    echo "<p> Nombre de vote : ".$row['note_Album']."</p>";
-    echo "<p> Pseudo des votants : ".$row['Pseudo_membre']."</p>";
-}
-$req ->closeCursor();
-
-
-
-// Requete affichage score votes catégorie STYLE HARDCORE PUNK:
-
-$req = $bd->query("SELECT nomGroupe_Groupe, nomStyleSecondaire_StyleSecondaire, nomStylePrincipal_StylePrincipal, note_Album, Pseudo_membre FROM groupe
-NATURAL JOIN album
-NATURAL JOIN stylesecondaire
-NATURAL JOIN styleprincipal
-NATURAL JOIN a_voté_pour
-NATURAL JOIN membre
-WHERE idStyleprincipal_StylePrincipal='3'
-ORDER BY note_Album");
-
-while ($row = $req->fetch()){
-    echo "<p> Nom du Groupe : ".$row['nomGroupe_Groupe']."</p>";
-    echo "<p> Style : ".$row['nomStyleSecondaire_StyleSecondaire']."</p>";
-    echo "<p> Catégorie : ".$row['nomStylePrincipal_StylePrincipal']."</p>";
-    echo "<p> Nombre de vote : ".$row['note_Album']."</p>";
-    echo "<p> Pseudo des votants : ".$row['Pseudo_membre']."</p>";
-}
-$req ->closeCursor();
-
-
-
-// Requete affichage score votes catégorie STYLE METAL HARD ROCK:
-
-$req = $bd->query("SELECT nomGroupe_Groupe, nomStyleSecondaire_StyleSecondaire, nomStylePrincipal_StylePrincipal, note_Album, Pseudo_membre FROM groupe
-NATURAL JOIN album
-NATURAL JOIN stylesecondaire
-NATURAL JOIN styleprincipal
-NATURAL JOIN a_voté_pour
-NATURAL JOIN membre
-WHERE idStyleprincipal_StylePrincipal='4'
-ORDER BY note_Album");
-
-while ($row = $req->fetch()){
-    echo "<p> Nom du Groupe : ".$row['nomGroupe_Groupe']."</p>";
-    echo "<p> Style : ".$row['nomStyleSecondaire_StyleSecondaire']."</p>";
-    echo "<p> Catégorie : ".$row['nomStylePrincipal_StylePrincipal']."</p>";
-    echo "<p> Nombre de vote : ".$row['note_Album']."</p>";
-    echo "<p> Pseudo des votants : ".$row['Pseudo_membre']."</p>";
-}
-$req ->closeCursor();
-
-
-
-// Requete affichage score votes catégorie STYLE METAL HEAVY TRASH:
-
-$req = $bd->query("SELECT nomGroupe_Groupe, nomStyleSecondaire_StyleSecondaire, nomStylePrincipal_StylePrincipal, note_Album, Pseudo_membre FROM groupe
-NATURAL JOIN album
-NATURAL JOIN stylesecondaire
-NATURAL JOIN styleprincipal
-NATURAL JOIN a_voté_pour
-NATURAL JOIN membre
-WHERE idStyleprincipal_StylePrincipal='5'
-ORDER BY note_Album");
-
-while ($row = $req->fetch()){
-    echo "<p> Nom du Groupe : ".$row['nomGroupe_Groupe']."</p>";
-    echo "<p> Style : ".$row['nomStyleSecondaire_StyleSecondaire']."</p>";
-    echo "<p> Catégorie : ".$row['nomStylePrincipal_StylePrincipal']."</p>";
-    echo "<p> Nombre de vote : ".$row['note_Album']."</p>";
-    echo "<p> Pseudo des votants : ".$row['Pseudo_membre']."</p>";
-}
-$req ->closeCursor();
-
-
-
-// Requete affichage score votes catégorie STYLE METAL FUSION:
-
-$req = $bd->query("SELECT nomGroupe_Groupe, nomStyleSecondaire_StyleSecondaire, nomStylePrincipal_StylePrincipal, note_Album, Pseudo_membre FROM groupe
-NATURAL JOIN album
-NATURAL JOIN stylesecondaire
-NATURAL JOIN styleprincipal
-NATURAL JOIN a_voté_pour
-NATURAL JOIN membre
-WHERE idStyleprincipal_StylePrincipal='6'
-ORDER BY note_Album");
-
-while ($row = $req->fetch()){
-    echo "<p> Nom du Groupe : ".$row['nomGroupe_Groupe']."</p>";
-    echo "<p> Style : ".$row['nomStyleSecondaire_StyleSecondaire']."</p>";
-    echo "<p> Catégorie : ".$row['nomStylePrincipal_StylePrincipal']."</p>";
-    echo "<p> Nombre de vote : ".$row['note_Album']."</p>";
-    echo "<p> Pseudo des votants : ".$row['Pseudo_membre']."</p>";
 }
 $req ->closeCursor();
