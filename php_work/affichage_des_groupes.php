ca61e44cfb696b14885c1ec39de667b71835f96d
ChrisL_frakkass <dcl.chrisl@18pixel.fr>
2019-02-11 15:21:08 +0100

    change picture on cat

--- php_work/affichage_des_groupes.php
+++ php_work/affichage_des_groupes.php
@@ -25,15 +25,19 @@ $req->execute(array(
     <title>Groupes du <?php echo $choix; ?></title>
 </head>
 <header>
-    <nav class="navbar navbar-expand-sm bg-dark navbar-dark fixed-top">
+    <nav class="navbar navbar-expand-sm bg-dark navbar-dark fixed-top justify-content-between">
         <a class="navbar-brand" href="#"><?php echo $choix; ?></a>
         <ul class="navbar-nav">
             <li class="nav-item">
             <a class="nav-link" href="./affichage_categorie.php">Retour</a>
             </li>
         </ul>
+
+    <button class="sm-5">Test</button>
     </nav>
 </header>
+
+*/
 <body class="position-absolute mt-4 mt-md-3">   
     <div class="mt-5 mt-sm-none contenair position-relative"></div>  <!-- div intercalaire -->
     <div class="row">
@@ -127,6 +131,6 @@ $req->execute(array(
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js" integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous"></script>
-    <script src="../js/script.js"></script>
+    <!-- <script src="../js/script.js"></script> -->
 </body>
 </html>
