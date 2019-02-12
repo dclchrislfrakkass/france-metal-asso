66bbd745d2f81ac941e46a4ba3cfdf382ce3e08f
dclonyx <dcl.fabieno@18pixel.fr>
2019-02-12 15:00:01 +0100

    affichage vote admin

--- php_work/affichage_vote.php
+++ php_work/affichage_vote.php
@@ -1,7 +1,15 @@
 <?php
 // Appel conexion a la base
-require '../php/pdo.php';
-// Requete affichage score vote total:
+require '../php/pdo.php'; ?>
+<!-- Requete affichage score vote total: -->
+
+<h1>Affichage des 3 groupes ayant le plus de vote toutes catégories confondues</h1>
+
+
+<?php 
+//comptage des votants total
+$req7 = $bd->query("SELECT count(*) FROM a_voté_pour");
+$voteTotal = $req7->fetch();
 
 $req = $bd->query("SELECT * FROM groupe
 NATURAL JOIN album
@@ -9,7 +17,7 @@ NATURAL JOIN stylesecondaire
 NATURAL JOIN styleprincipal
 NATURAL JOIN a_voté_pour
 GROUP BY nomGroupe_Groupe
-ORDER BY note_Album DESC");
+ORDER BY note_Album DESC LIMIT 3");
 
 while ($row = $req->fetch()){
     $chaine='';
@@ -19,25 +27,23 @@ while ($row = $req->fetch()){
     NATURAL JOIN a_voté_pour
     NATURAL JOIN membre
     WHERE idAlbum_Album = :idalbum
-    ORDER BY note_Album DESC
-    LIMIT 3");
+    ORDER BY note_Album DESC");
     $req2->execute(array(
         'idalbum' => $row['idAlbum_Album']
     ));
     while ($row2 = $req2->fetch()){
-        // var_dump($req2);
-        $chaine = $row2['Pseudo_membre']." ".$chaine;
+        $chaine = $row2['Pseudo_membre'].", ".$chaine;
         $chaine2 = $row2['idMembre_membre']." ".$chaine2;
-
     }
-
-    echo "<p> Nom du Groupe : ".$row['nomGroupe_Groupe']."</p>";
-    echo "<p> id album: ".$row['idAlbum_Album']."</p>";
-    echo "<p> id membre : ".$chaine2."</p>";
-    echo "<p> Style : ".$row['nomStyleSecondaire_StyleSecondaire']."</p>";
-    echo "<p> Catégorie : ".$row['nomStylePrincipal_StylePrincipal']."</p>";
-    echo "<p> Nombre de vote : ".$row['note_Album']."</p>";
-    echo "<p> Pseudo des votants : ".$chaine."</p><br>";
-
+    echo "<p><strong> Nom du Groupe : </strong>".$row['nomGroupe_Groupe']."</p>";
+    echo "<p><strong> id album : </strong>".$row['idAlbum_Album']."</p>";
+    echo "<p><strong> Nombre de vote : </strong>".$row['note_Album']." / ".$voteTotal['count(*)']."</p>";
+    // echo "<p><strong> id  des votants : </strong>".$chaine2."</p>";
+    echo "<p><strong> Style : </strong>".$row['nomStyleSecondaire_StyleSecondaire']."</p>";
+    echo "<p><strong> Catégorie : </strong>".$row['nomStylePrincipal_StylePrincipal']."</p>";
+    echo "<p><strong> Pseudo des votants : </strong>".$chaine."</p><br>";
 }
 $req ->closeCursor();
+$req7 ->closeCursor();
+
+include './traitement_votes.php';
---
+++ php_work/traitement_votes.php
@@ -0,0 +1,59 @@
+
+ <?php 
+// Requete catégorie STYLE HARDCORE PUNK:
+$requetestyle = $bd -> prepare("SELECT count(idStyleprincipal_StylePrincipal) AS NBID FROM styleprincipal");
+$requetestyle -> execute();
+$row = $requetestyle->fetch();
+$NBID = $row['NBID'];
+
+    for ($ccount = 1 ; $ccount <= intval($NBID) ; $ccount++ ){
+       
+        // var_dump($req10);
+        $requetephoto = $bd->prepare("SELECT * FROM styleprincipal
+        WHERE idStyleprincipal_StylePrincipal=:ccount
+        ");
+        $requetephoto-> execute(array(
+            'ccount' => $ccount
+        ));
+        $row = $requetephoto->fetch();
+        echo "<img src='".$row["illustration"]."' style='width:15%;'>";
+
+        $req = $bd->prepare("SELECT * FROM groupe
+        NATURAL JOIN album
+        NATURAL JOIN stylesecondaire
+        NATURAL JOIN styleprincipal
+        NATURAL JOIN a_voté_pour
+        WHERE idStyleprincipal_StylePrincipal=:ccount
+        GROUP BY nomGroupe_Groupe
+        ORDER BY note_Album DESC");
+        $req -> execute(array(
+            'ccount' => $ccount
+        ));
+        $row = $req->fetch();
+
+        if(empty($row['note_Album'])){
+                echo '<p>Aucun vote dans cette catégorie</p>';
+            } else {
+                $req = $bd->prepare("SELECT * FROM groupe
+                NATURAL JOIN album
+                NATURAL JOIN stylesecondaire
+                NATURAL JOIN styleprincipal
+                NATURAL JOIN a_voté_pour
+                WHERE idStyleprincipal_StylePrincipal=:ccount
+                GROUP BY nomGroupe_Groupe
+                ORDER BY note_Album DESC");
+                $req -> execute(array(
+                    'ccount' => $ccount
+                ));
+                while (!empty($row = $req->fetch())){
+                    echo "<p><strong> Nom du Groupe : </strong>".$row['nomGroupe_Groupe']."</p>";
+                    // echo "<p><strong> Nombre de vote : </strong>".$row3['note_Album']." / ".$voteTotal['count(*)']."</p>"; // remplacer plus tard le vote total par le vote total de sa catégorie
+                    echo "<p><strong> Style : </strong>".$row['nomStyleSecondaire_StyleSecondaire']."</p><br>";
+                    // echo "<p><strong> Pseudo des votants : </strong>".$chaine."</p><br>";
+                }
+            }
+        $requetephoto->closeCursor();
+        $req->closeCursor();
+        }
+        ?>
+  
