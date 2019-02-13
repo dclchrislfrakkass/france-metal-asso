e1a43762c1b9bd595459db52da89615779d11e38
Sicca <dcl.laurentj@18pixel.fr>
2019-02-13 10:58:58 +0100

    maj wordpress

--- theme enfant/twentyseventeen/page.php
+++ theme enfant/twentyseventeen/page.php
@@ -20,10 +20,10 @@ get_header(); ?>
 <div class="wrap">
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
-
 			<?php
 			while ( have_posts() ) :
 				the_post();
+				echo "COUCOU";
 
 				get_template_part( 'template-parts/page/content', 'page' );
 
