8320f6539d7e230bcf129f15ff5838bba4dd94f5
Sicca <dcl.laurentj@18pixel.fr>
2019-02-18 11:37:09 +0100

    maj ok

--- theme enfant/twentyseventeen-child/template.php
+++ theme enfant/twentyseventeen-child/template.php
@@ -15,16 +15,11 @@
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js" integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous"></script>
-<<<<<<< HEAD
     
     <script>
 
     var checkbox = document.querySelectorAll("[type=checkbox]");
 
-=======
-    <script src="<?php echo get_template_directory_uri(); ?>-child/script.js">
-    var checkbox = document.querySelectorAll("[type=checkbox]");
->>>>>>> 18f88fb94314677093b01ebd91236be3fb3022e3
     var checkboxlenght = checkbox.length;
     var longueur = <?php echo json_encode($longueur); ?>;
 
@@ -35,12 +30,8 @@
                 alert('3 votes maximum svp');
             }  
         }, false);
-<<<<<<< HEAD
     };
 
-=======
-    };    
->>>>>>> 18f88fb94314677093b01ebd91236be3fb3022e3
     </script>
 </body>
 </html>
