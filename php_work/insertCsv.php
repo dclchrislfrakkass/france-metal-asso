916b557f2050d0a81c8ca0bbe5e4ca48538ef174
ChrisL_frakkass <dcl.chrisl@18pixel.fr>
2019-02-19 11:57:51 +0100

    insertCSV base

--- php_work/insertCsv.php
+++ php_work/insertCsv.php
@@ -7,18 +7,30 @@ ob_start();
 
 
 ?>
-<body class="position-absolute mt-sm-3 mt-5 pt-2">   
-    <h1 class="text-center">Ajout de nouvelles entrées dans la base de données</h1>
-<div class="mt-5 mt-sm-none contenair position-relative">
+<body class="position-absolute mt-sm-3 mt-5 pt-2">
+    <h1 class="text-center"> Insertion dans la base de données de nouvelles entrées</h1>
+    <div class="mt-5 mt-sm-none contenair position-relative">
+        
 
+    <div class="form-group col-12 align-content-around">
+        <label class="row" for="inputStyleP">Choix du Style Principal</label>
+        <select name='styleP' id='styleP' class="custom-select col-sm-12 col-lg-8 mb-5">
+            <option value='-1'>Aucun</option>
+            <?php
+               
+            $req = $bd->query("SELECT * FROM styleprincipal");
+            while ($row = $req->fetch()){
+                   
+                echo "<option value='".$row['idStylePrincipal_StylePrincipal']."'>".$row['nomStylePrincipal_StylePrincipal']."</option>";
+                }
+                
+                ?>
+        </select>
 
-<form class="row d-flex justify-content-center">
 <div class="form-row col-12 md-12 col-lg-8">
-
-<div class="form-row col-12 md-12 col-lg-8">
-<div class="custom-file form-group col-md-12">
+<div class="custom-file form-group col-md-12 mb-5">
     <input type="file" class="custom-file-input" id="csvFile">
-    <label class="custom-file-label" for="customFile">Choisissez votre fichier CSV</label>
+    <label class="custom-file-label" for="customFile">Choisissez votre fichier</label>
     </div>
   
 
@@ -26,12 +38,11 @@ ob_start();
 
 
 </div>
-<br><br>
-                <div class="row">
+                <div class="row d-flex justify-content-center col-8">
     <button type="submit" class="btn btn-danger">Ajouter</button>
     </div>   
 </form>
-
+            </div>
 <?php
 
 $content = ob_get_clean();
