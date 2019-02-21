c9d7c3962967e5af5365701032d3ff2ce516faed
dclonyx <dcl.fabieno@18pixel.fr>
2019-02-21 15:18:05 +0100

    injection groupe

--- php/pdo.php
+++ php/pdo.php
@@ -1,7 +1,7 @@
 <?php
 
 try {
-$json =file_get_contents('../pdo.JSON');
+$json =file_get_contents('../php/pdo.JSON');
 $dec=json_decode($json, true);
 
 $bd=new PDO("mysql:host=".$dec['host'].";dbname=".$dec['dbName'], $dec['user'] , $dec['pass']);
